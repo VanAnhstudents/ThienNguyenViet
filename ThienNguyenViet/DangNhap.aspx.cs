389408c1055293263a2f64e40563574a10f3d5ff
Quy12351 <trautre715@gmail.com>
2026-03-23 20:22:25 +0700

    DangNhap.aspx.cs

--- ThienNguyenViet/DangNhap.aspx.cs
+++ ThienNguyenViet/DangNhap.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +11,29 @@ namespace ThienNguyenViet
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                lblError.Visible = false;
+            }
+        }
 
+        protected void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            // Demo - bạn có thể thay bằng kiểm tra Database sau này
+            string email = txtEmail.Text.Trim();
+            string pass = txtPassword.Text.Trim();
+
+            if (email == "user@example.com" && pass == "123456")
+            {
+                Session["UserEmail"] = email;
+                Session["IsLoggedIn"] = true;
+                Response.Redirect("Default.aspx"); // ← Thay bằng trang chủ của bạn
+            }
+            else
+            {
+                lblError.Text = "Email hoặc mật khẩu không đúng!";
+                lblError.Visible = true;
+            }
         }
     }
-}
+}﻿
