f91d16ebe4237f99f8b31574da761e929cd3baab
Quy12351 <trautre715@gmail.com>
2026-03-23 20:01:54 +0700

    DangKy.aspx.cs

--- ThienNguyenViet/DangKy.aspx.cs
+++ ThienNguyenViet/DangKy.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +11,27 @@ namespace ThienNguyenViet
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack) lblError.Visible = false;
+        }
 
+        protected void btnDangKy_Click(object sender, EventArgs e)
+        {
+            // Validation server-side (bổ sung cho JS)
+            if (string.IsNullOrWhiteSpace(txtHoTen.Text) ||
+                string.IsNullOrWhiteSpace(txtEmail.Text) ||
+                string.IsNullOrWhiteSpace(txtSoDienThoai.Text) ||
+                txtMatKhau.Text != txtXacNhanMK.Text ||
+                txtMatKhau.Text.Length < 6 ||
+                !chkDongY.Checked)
+            {
+                lblError.Text = "Vui lòng kiểm tra lại thông tin!";
+                lblError.Visible = true;
+                return;
+            }
+
+            // Thành công → chuyển về trang đăng nhập
+            Session["RegisterSuccess"] = "Đăng ký thành công! Vui lòng đăng nhập.";
+            Response.Redirect("DangNhap.aspx");
         }
     }
 }
